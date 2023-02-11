1b7b03dc9286ce9d1384565aa649bb9b49e76d2b
Denan Delic <denandelic@Denans-MacBook-Pro.local>
2023-02-11 01:23:08 +0100

    first commit

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {google, slack, atlassian, dropbox, shopify} from './imports'
+import './brand.css';
+import { RiGoogleLine } from 'react-icons/ri';
+
+const Brand = () => {
+  return (
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt='google' />
+      </div>
+      <div>
+        <img src={slack} alt='slack' />
+      </div>
+      <div>
+        <img src={atlassian} alt='atlassian' />
+      </div>
+      <div>
+        <img src={atlassian} alt='atalassian' />
+      </div>
+      <div>
+        <img src={dropbox} alt='dropbox' />
+      </div>
+
+    </div>
+  )
+}
+
+export default Brand
